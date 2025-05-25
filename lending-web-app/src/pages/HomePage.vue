2776d3db44ff2a3778e18f4d379db131a74d1246
DESKTOP-E6U75VE <anya-batanova>
2025-05-25 03:28:13 +0300

    Добавила кнопку для скролла наверх страницы

--- lending-web-app/src/pages/HomePage.vue
+++ lending-web-app/src/pages/HomePage.vue
@@ -7,6 +7,26 @@ import HeroSection from '@/components/home/HeroSection.vue'
 import HowWeLearn from '@/components/home/HowWeLearn.vue'
 import LearnSteps from '@/components/home/LearnSteps.vue'
 import Statistics from '@/components/home/Statistics.vue'
+
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const showScrollButton = ref(false)
+
+const handleScroll = () => {
+  showScrollButton.value = window.scrollY > window.innerHeight
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
 </script>
 
 <template>
@@ -24,4 +44,12 @@ import Statistics from '@/components/home/Statistics.vue'
   </div>
 
   <FooterComponent />
+  <!-- Кнопка прокрутки вверх -->
+  <button
+    v-if="showScrollButton"
+    @click="scrollToTop"
+    class="fixed bottom-8 right-8 bg-base-blue hover:bg-base-light-blue text-black font-w800 p-3 rounded-full shadow-lg transition-opacity duration-300"
+  >
+    <img src="/icons/arrow-up.svg" alt="icon" class="w-8 h-8" />
+  </button>
 </template>
