197e6f449b0d542e8420aea91d1bd95529ca4cee
DESKTOP-E6U75VE <anya-batanova>
2025-05-25 03:33:16 +0300

    Поправила адаптивность статистики

--- lending-web-app/src/components/home/Statistics.vue
+++ lending-web-app/src/components/home/Statistics.vue
@@ -21,11 +21,11 @@ const statistics = [
 <template>
   <div class="bg-cover bg-center" style="background-image: url('/books.webp')">
     <div
-      class="py-20 bg-gradient-to-b from-base-gradient-blue/90 to-base-gradient-dark-blue/90 text-white"
+      class="py-5 lg:py-24 bg-gradient-to-b from-base-gradient-blue/90 to-base-gradient-dark-blue/90 text-white"
     >
-      <div class="lg:max-w-6xl grid md:grid-cols-3 gap-8 text-center mx-auto">
-        <div v-for="stat in statistics" :key="stat.id" class="p-6">
-          <div class="max-w-[300px]">
+      <div class="lg:max-w-6xl grid md:grid-cols-3 lg:gap-8 text-center mx-auto">
+        <div v-for="stat in statistics" :key="stat.id" class="p-8 lg:p-0">
+          <div class="mx-auto max-w-[300px]">
             <div class="text-7xl font-w600 mb-8">{{ stat.value }}</div>
             <p class="text-md leading-[28px]">{{ stat.description }}</p>
           </div>
